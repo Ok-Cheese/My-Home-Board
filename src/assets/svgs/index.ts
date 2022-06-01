97cdaea352472bac37f73d8e4cc75cba02ee3ff5
Ok-Cheese <starberry970310@gmail.com>
2022-06-02 00:21:22 +0900

    feat: add icon in assets/svgs and create index.ts for import

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,3 @@
+export { ReactComponent as SearchIcon } from './search.svg';
+export { ReactComponent as GoogleIcon } from './google.svg';
+export { ReactComponent as YoutubeIcon } from './youtube.svg';
