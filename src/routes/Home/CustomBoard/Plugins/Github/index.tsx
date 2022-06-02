1f02ad0a9f247b39e64fbf6c470cfe031f9d83b4
Ok-Cheese <starberry970310@gmail.com>
2022-06-02 23:20:28 +0900

    feat: create github plugin

--- src/routes/Home/CustomBoard/Plugins/Github/index.tsx
+++ src/routes/Home/CustomBoard/Plugins/Github/index.tsx
@@ -1,12 +1,9 @@
 import { VictoryAxis, VictoryChart, VictoryLine } from 'victory';
-import axios from 'axios';
 import dayjs from 'dayjs';
 
 import { GitHubIcon } from 'assets/svgs';
 
 import { testData } from './test_data';
-
-import styles from 'github.module.scss';
 
 const Github = () => {
   /* axios
@@ -24,33 +21,50 @@ const Github = () => {
       console.log(filtered);
     }); */
 
-  const eventData = testData
-    .filter((el: any) => {
-      const lastWeek = Number(dayjs().subtract(6, 'day').format('YYMMDD'));
-      const eventDate = Number(dayjs(el.created_at.slice(0, 10)).format('YYMMDD'));
+  const convertGitHubData = (data: any, numOfDays: number) => {
+    const justNumbers = Array(numOfDays)
+      .fill(0)
+      .map((el, i) => i);
 
-      return el.type === 'PushEvent' && lastWeek < eventDate;
-    })
-    .map((el: any) => {
-      return {
-        y: el.payload.size,
-        x: dayjs(el.created_at).format('MMDD'),
-      };
-    })
-    .reverse();
+    const lastWeek = justNumbers.map((num) => dayjs().subtract(num, 'day').format('MMDD')).reverse();
 
-  console.log(eventData);
+    const filteredData = data.filter((el: any) => {
+      const eventDate = Number(dayjs(el.created_at.slice(0, 10)).format('MMDD'));
+
+      return el.type === 'PushEvent' && Number(lastWeek[0]) <= eventDate;
+    });
+
+    const commits = Array(numOfDays).fill(0);
+
+    filteredData.forEach((filtData: any) => {
+      const eventDate = dayjs(filtData.created_at).format('MMDD');
+
+      lastWeek.forEach((date, index) => {
+        if (eventDate === date) {
+          commits[index] += filtData.payload.size;
+        }
+      });
+    });
+
+    const resultData = commits.map((commit, index) => {
+      return { x: lastWeek[index], y: commit };
+    });
+
+    return resultData;
+  };
+
+  const eventData = convertGitHubData(testData, 7);
 
   const getMaxValue = () => {
     const maximum = Math.max(...eventData.map((el) => el.y));
-    const unit = maximum.toString().length;
+    const len = maximum.toString().length - 1;
+    const unit = 10 ** len;
     const roundedUp = Math.ceil(maximum / unit) * unit;
-    const adjustValue = [roundedUp, roundedUp / 2, roundedUp / 10].map((el) => el - maximum).filter((el) => el >= 0);
 
-    return Math.min(...adjustValue);
+    return roundedUp;
   };
 
-  const intervalValues = [0.2, 0.4, 0.6, 0.8, 1].map((el) => el * getMaxValue());
+  const intervalValues = [0.2, 0.4, 0.6, 0.8, 1].map((el) => Math.ceil(el * getMaxValue()));
 
   return (
     <div>
@@ -58,9 +72,9 @@ const Github = () => {
         <GitHubIcon />
         <h2>Github Events</h2>
       </div>
-      <VictoryChart domainPadding={20}>
+      <VictoryChart domainPadding={{ x: 10 }}>
         <VictoryLine data={eventData} />
-        <VictoryAxis tickCount={7} tickFormat={(x) => x} />
+        <VictoryAxis tickCount={8} tickFormat={(x) => x} />
         <VictoryAxis dependentAxis tickValues={intervalValues} />
       </VictoryChart>
     </div>
