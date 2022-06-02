9992418ce17876beafbcf065be6f5b06ff9636d7
unknown <starberry970310@gmail.com>
2022-06-02 15:16:58 +0900

    feat: add github, setting, help icons

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,3 +1,6 @@
 export { ReactComponent as SearchIcon } from './search.svg';
 export { ReactComponent as GoogleIcon } from './google.svg';
 export { ReactComponent as YoutubeIcon } from './youtube.svg';
+export { ReactComponent as GitHubIcon } from './github.svg';
+export { ReactComponent as HelpIcon } from './help.svg';
+export { ReactComponent as SettingIcon } from './setting.svg';
