a515af336bc34fafc6ae93fd0b5a6dc878c1d401
Ok-Cheese <starberry970310@gmail.com>
2022-06-09 15:48:34 +0900

    feat: add reload icon

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -12,3 +12,4 @@ export { ReactComponent as CheckboxIcon } from './checkbox.svg';
 export { ReactComponent as BlankBoxIcon } from './blankBox.svg';
 export { ReactComponent as CloseIcon } from './close.svg';
 export { ReactComponent as ArrowIcon } from './arrow.svg';
+export { ReactComponent as ReloadIcon } from './reload.svg';
