46f8fe14346a1fdea025a132261137f7589950c8
Ok-Cheese <starberry970310@gmail.com>
2022-06-10 22:33:46 +0900

    feat: add Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,18 @@
+import { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import styles from './input.module.scss';
+
+interface IProps {
+  type?: 'text' | 'password';
+  value: string;
+  setValue: Dispatch<SetStateAction<string>>;
+}
+
+const Input = ({ type, value, setValue }: IProps) => {
+  const changeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value);
+  };
+
+  return <input type={type || 'text'} className={styles.input} value={value} onChange={changeInputHandler} />;
+};
+
+export default Input;
