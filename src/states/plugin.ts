e37787f909514a6715f00d045b3a64179bb2dcbf
Ok-Cheese <starberry970310@gmail.com>
2022-07-17 01:19:42 +0900

    fix: change minimum height BoxOffice

--- src/states/plugin.ts
+++ src/states/plugin.ts
@@ -5,13 +5,13 @@ import store from 'store';
 const totalPlugins: Layout[] = [
   { i: 'search', w: 3, h: 1, x: 0, y: 0, maxH: 1, resizeHandles: ['se', 'sw', 'nw'] },
   { i: 'bookmark', w: 3, h: 3, x: 0, y: 0, minW: 2, minH: 3, resizeHandles: ['se', 'sw', 'nw'] },
-  { i: 'setting', w: 1, h: 1, x: 0, y: 0, resizeHandles: [] },
+  { i: 'setting', w: 1, h: 1, x: 0, y: 0 },
   { i: 'todoList', w: 2, h: 4, x: 0, y: 0, minW: 2, minH: 4, resizeHandles: ['se', 'sw', 'nw'] },
   { i: 'time', w: 2, h: 2, x: 0, y: 0, minW: 2, minH: 2, resizeHandles: ['se', 'sw', 'nw'] },
   { i: 'clock', w: 2, h: 4, x: 0, y: 0, minW: 2, minH: 4, maxW: 2, maxH: 4 },
   { i: 'weather', w: 2, h: 2, x: 0, y: 0, minW: 2, minH: 2, resizeHandles: ['se', 'sw', 'nw'] },
   { i: 'dday', w: 2, h: 2, x: 0, y: 0, minW: 2, minH: 2, resizeHandles: ['se', 'sw', 'nw'] },
-  { i: 'boxOffice', w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3, resizeHandles: ['se', 'sw', 'nw'] },
+  { i: 'boxOffice', w: 2, h: 5, x: 0, y: 0, minW: 2, minH: 5, resizeHandles: ['se', 'sw', 'nw'] },
 ];
 
 const initialLayoutList: Layout[] = [
