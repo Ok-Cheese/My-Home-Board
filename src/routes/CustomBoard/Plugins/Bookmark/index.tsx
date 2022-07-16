080fb11081b88bee1562dc2c294d74abe538923a
Ok-Cheese <starberry970310@gmail.com>
2022-07-17 02:10:53 +0900

    fix: remove unused values

--- src/routes/CustomBoard/Plugins/Bookmark/index.tsx
+++ src/routes/CustomBoard/Plugins/Bookmark/index.tsx
@@ -1,5 +1,4 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
-import { Layout } from 'react-grid-layout';
 import HorizontalScroll from 'react-scroll-horizontal';
 import store from 'store';
 
@@ -14,13 +13,9 @@ import Icon from 'components/Icon';
 
 import styles from './bookmarks.module.scss';
 
-interface IProps {
-  layout: Layout;
-}
-
 const savedBookmark = store.get('bookmarkList');
 
-const Bookmarks = ({ layout }: IProps) => {
+const Bookmarks = () => {
   const [bookmarkList, setBookmarkList] = useState<IBookmark[]>(savedBookmark || bookmarkPreset);
   const [isModalOpened, setIsModalOpened] = useState(false);
   const [isRemoveMode, setIsRemoveMode] = useState(false);
