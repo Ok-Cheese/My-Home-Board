aac5e8645e1fa0d20bbbfc23bda5bf907fa7817e
Ok-Cheese <starberry970310@gmail.com>
2022-07-16 23:41:02 +0900

    feat: change structure

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,23 +4,29 @@ import styles from './input.module.scss';
 
 interface IProps {
   type?: 'text' | 'password';
+  label?: string;
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
 }
 
-const Input = ({ type, value, setValue }: IProps) => {
+const Input = ({ type, label, value, setValue }: IProps) => {
   const changeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.currentTarget.value);
   };
 
   return (
-    <input
-      type={type || 'text'}
-      className={styles.input}
-      value={value}
-      onChange={changeInputHandler}
-      spellCheck={false}
-    />
+    <fieldset className={styles.fieldset}>
+      <label className={styles.label}>
+        {label}
+        <input
+          type={type || 'text'}
+          className={styles.input}
+          value={value}
+          onChange={changeInputHandler}
+          spellCheck={false}
+        />
+      </label>
+    </fieldset>
   );
 };
 
